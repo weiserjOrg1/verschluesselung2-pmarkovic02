8d4941e5bea2a0a4abc361b06927d5475e4a896a
Philip Markovic <pmarkovic02@student.tgm.ac.at>
2018-10-12 12:59:30 +0200

    Second Commit mit Example Class

--- src/example/ExampleClass.java
+++ src/example/ExampleClass.java
@@ -4,9 +4,8 @@ public class ExampleClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
+		String test="TESTMELDUNG";
+		System.out.println(test);
 	}
 
 }
